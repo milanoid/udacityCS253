94d9ce0e050c85db275c6c904f9f4230e9ecc9cb
Milan Vojnovic <milanvojnovic@gmail.com>
2013-04-07 18:55:53 +0200

    Naive caching
    Naive caching (before memcache)

---
+++ milancs253unit6/mainLesson6.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import jinja2
+import webapp2
+import urllib2
+import logging
+from xml.dom import minidom
+
+
+from google.appengine.ext import db
+from google.appengine.api import memcache
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+GMAPS_URL = "http://maps.googleapis.com/maps/api/staticmap?size=380x263&sensor=false&"
+def gmaps_img(points):
+	markers = '&'.join('markers=%s,%s' % (p.lat, p.lon) for p in points)
+	return GMAPS_URL + markers
+
+IP_URL = "http://api.hostip.info/?ip="
+def get_coords(ip):
+	ip = "4.2.2.2"
+	url = IP_URL + ip
+	content = None
+	try:
+		content = urllib2.urlopen(url).read()
+	except URLError:
+		return
+
+	if content:
+		# parse the xml and find the coordinates
+		d = minidom.parseString(content)
+		coords = d.getElementsByTagName("gml:coordinates")
+		if coords and coords[0].childNodes[0].nodeValue:
+			lon, lat = coords[0].childNodes[0].nodeValue.split(',')
+			return db.GeoPt(lat, lon)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class Art(db.Model):
+	title = db.StringProperty(required = True)
+	art = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+	coords = db.GeoPtProperty()
+
+CACHE = {}
+def top_arts(update = False):
+	key = 'top'
+
+	if not update and key in CACHE:
+		arts = CACHE[key]
+	else:
+		logging.error("DB QUERY")
+		arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC LIMIT 10")
+		arts = list(arts)
+		CACHE[key] =  arts
+	return arts
+
+
+class MainPage(Handler):
+	def render_front(self, title="", art="", error=""):
+		arts = top_arts()
+
+		# find which arts have coords
+		points = filter(None, (a.coords for a in arts))
+
+		# if we have any arts coords, make an image url
+		img_url = None
+		if points:
+			img_url = gmaps_img(points)
+		
+		# dispaly the image url
+		self.render("front.html", title=title, art=art, error=error, arts = arts, img_url = img_url)
+
+
+	def get(self):
+		#self.write(self.request.remote_addr)
+		self.write(repr(get_coords(self.request.remote_addr)))
+		self.render_front()
+
+	def post(self):
+		title = self.request.get("title")
+		art = self.request.get("art")
+
+		if title and art:
+			a = Art(title = title, art = art)
+			# lookup the user's coordinates from their IP
+			coords = get_coords(self.request.remote_addr)
+			# if we have coordinates, add them to the Art
+			if coords:
+				a.coords = coords
+
+
+			a.put()
+			# clear the cache right after writing in a new art
+			#CACHE.clear() 
+
+			# update cache
+			top_arts(True)
+
+			self.redirect("/")
+		else:
+			error = "we need both a title and some artwork!"
+			self.render_front(title, art, error)
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
